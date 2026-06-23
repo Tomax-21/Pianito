06e4d5bb36b97c2870ae76403ccb46724ab3f1a3
Tomax-21 <thom21fr@gmail.com>
2026-06-23 21:15:07 +0200

    feat : creation du component de piano

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Piano } from "./components/piano/piano"
 
 function App() {
 
   return (
     <>
       <h1>Hello world</h1>
+      <Piano/>
     </>
   )
 }
---
+++ src/components/piano/piano.tsx
@@ -0,0 +1,5 @@
+export function Piano() {
+    return (
+        <div>Test piano</div>
+    )
+}
