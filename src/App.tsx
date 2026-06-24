dc7366ec55bd1b7bef2d0c0ba6eef7e5db4fc956
Tomax-21 <thom21fr@gmail.com>
2026-06-24 21:38:07 +0200

    feat : recuperation de la note jouer jusqu'a l'app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { Partition } from "./components/partition/partition"
 import { Piano } from "./components/piano/piano"
+import { SoundPlayer } from "./components/sound_player"
 
 function App() {
+  const handlePlayedNote = (noteName: string) => {
+    SoundPlayer(noteName)
+  }
 
   return (
     <>
     <div className="piano-partition-page">
       <div className="partition-box">
         <h2>Partition</h2>
-        <Partition notes_list={["C", "D", "Eb", "F", "G", "G#"]}/>
+        <Partition notes_list={["C4", "D4", "Eb4", "F4", "G4", "G#4"]}/>
       </div>
         
-        <Piano/>
+        <Piano onNotePlayed={handlePlayedNote}/>
       </div>
     </>
   )
--- src/components/piano/piano.tsx
+++ src/components/piano/piano.tsx
@@ -1,22 +1,19 @@
 import "../../css/piano.css"
-import { SoundPlayer } from "../sound_player"
+import type {  PianoProps } from "../piano_types"
 import { PianoOctave } from "./pianoOctave"
 
-export function Piano() {
-    const handlePlayNote = (noteName: string) => {
-        SoundPlayer(noteName)
-    }
+export function Piano({onNotePlayed}: PianoProps) {
     return (
         <div className="piano">
-            <PianoOctave octave_number={0} is_first_octave={true} onNoteTriggered={handlePlayNote}/>
-            <PianoOctave octave_number={1} onNoteTriggered={handlePlayNote}/>
-            <PianoOctave octave_number={2} onNoteTriggered={handlePlayNote}/>
-            <PianoOctave octave_number={3} onNoteTriggered={handlePlayNote}/>
-            <PianoOctave octave_number={4} onNoteTriggered={handlePlayNote}/>
-            <PianoOctave octave_number={5} onNoteTriggered={handlePlayNote}/>
-            <PianoOctave octave_number={6} onNoteTriggered={handlePlayNote}/>
-            <PianoOctave octave_number={7} onNoteTriggered={handlePlayNote}/>
-            <PianoOctave octave_number={8} is_last_octave={true} onNoteTriggered={handlePlayNote}/>
+            <PianoOctave octave_number={0} is_first_octave={true} onNoteTriggered={onNotePlayed}/>
+            <PianoOctave octave_number={1} onNoteTriggered={onNotePlayed}/>
+            <PianoOctave octave_number={2} onNoteTriggered={onNotePlayed}/>
+            <PianoOctave octave_number={3} onNoteTriggered={onNotePlayed}/>
+            <PianoOctave octave_number={4} onNoteTriggered={onNotePlayed}/>
+            <PianoOctave octave_number={5} onNoteTriggered={onNotePlayed}/>
+            <PianoOctave octave_number={6} onNoteTriggered={onNotePlayed}/>
+            <PianoOctave octave_number={7} onNoteTriggered={onNotePlayed}/>
+            <PianoOctave octave_number={8} is_last_octave={true} onNoteTriggered={onNotePlayed}/>
         </div>
     )
 }
