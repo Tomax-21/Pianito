ee1cae3f7ef46e448e957f969237011c123084ab
Tomax-21 <thom21fr@gmail.com>
2026-06-24 22:00:23 +0200

    feat : recuperation d'une note au hasard

--- src/components/random.ts
+++ src/components/random.ts
@@ -3,3 +3,30 @@ export function random_number(min: number, max: number) : number {
 }
 
 
+export function get_random_note(note_range: Array<string> = ['A', 'B', 'C', 'D', 'E', 'F', 'G'], octave_range: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8]):string|null {
+
+    let octave:number = octave_range[random_number(0, octave_range.length)]
+
+    if (octave == 0) {
+        if (note_range.includes("A")) {
+            if (note_range.includes("B")) {
+                let possibilities = ['A', 'B']
+                return possibilities[random_number(0,1)]
+            } else {
+                return "A/0"
+            }
+        } else if (note_range.includes("B")) {
+            return "B/0"
+        } else {
+            return null
+        }
+    } else if (octave == 8) {
+        if (note_range.includes("C")) {
+            return 'C/8'
+        } else {
+            return null
+        }
+    }
+
+    return `${note_range[random_number(0, note_range.length)]}/${octave}`
+}
