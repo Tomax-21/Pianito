abb0a6b1e26fb29e6f0c6c373c278491eaf27df4
Tomax-21 <thom21fr@gmail.com>
2026-06-25 15:44:58 +0200

    feat : fonction pour verifier si le format des notes est correct

--- src/utils/note_comparison.ts
+++ src/utils/note_comparison.ts
@@ -1,3 +1,5 @@
+import { vexflowNoteToClassicNote } from "./note_conversion";
+
 //note au format classique en entrer
 const ENHARMONIC_MAP: Record<string, string> = {
     "C#": "Db", "Db": "C#",
@@ -8,6 +10,14 @@ const ENHARMONIC_MAP: Record<string, string> = {
 };
 
 export function areEnharmonic(note1:string, note2:string) {
+    if (!isNoteSyntaxClassic(note1) || !isNoteSyntaxClassic(note2)) {
+        if (isNoteSyntaxVexFlow(note1) && isNoteSyntaxVexFlow(note2)) {
+            note1 = vexflowNoteToClassicNote(note1).toUpperCase()
+            note2 = vexflowNoteToClassicNote(note2).toUpperCase()
+        }else {
+            return false
+        }
+    }
 
     if (note1 === note2) return true
 
@@ -18,6 +28,18 @@ export function areEnharmonic(note1:string, note2:string) {
     const octaveB = note2.slice(-1)
 
     if (octaveA !== octaveB) return false
-    
+
     return ENHARMONIC_MAP[nameA] === nameB
+}
+
+
+export function isNoteSyntaxClassic(note: string):boolean {
+    const classicNoteRegex = /^[A-G][#b]?[0-8]$/;
+
+    return classicNoteRegex.test(note)
+}
+
+export function isNoteSyntaxVexFlow(note : string): boolean {
+    const vexflowRegex = /^[a-g][#b]?\/[0-8]$/
+    return vexflowRegex.test(note)
 }
