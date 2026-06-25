11f2d9b75ce2eccc571998942f0226229c76503d
Tomax-21 <thom21fr@gmail.com>
2026-06-25 16:42:27 +0200

    feat, refactor : creation d'une fonction pour initialiser le joueur de son

--- src/components/piano/sound_player.ts
+++ src/components/piano/sound_player.ts
@@ -2,14 +2,18 @@ import * as Tone from "tone";
 
 let synth: Tone.Synth | null = null;
 
+export function init_sound_player_synth() {
+    synth = new Tone.Synth().toDestination()
+}
+
 export async function SoundPlayer(noteName:string, duration:string = "8n") {
     if (!synth) {
-        synth = new Tone.Synth().toDestination()
+        init_sound_player_synth()
     }
     if (Tone.getContext().state !== "running") { //redemarre en cas de prblm
         await Tone.start()
     }
 
     
-    synth.triggerAttackRelease(noteName, duration);
+    synth!.triggerAttackRelease(noteName, duration);
 }
