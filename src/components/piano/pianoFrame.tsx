4af8a9b239d88865224df9b7fa219220d6e63ca0
Tomax-21 <thom21fr@gmail.com>
2026-06-26 15:15:34 +0200

    style : ajout du bouton micro a la frame du piano

--- src/components/piano/pianoFrame.tsx
+++ src/components/piano/pianoFrame.tsx
@@ -1,9 +1,10 @@
-import { HelpCircle, RefreshCw } from "lucide-react";
+import { HelpCircle, Mic, RefreshCw } from "lucide-react";
 import type { PianoFrameProps } from "./piano_types";
 
-export function PianoFrame({onHelpButtonClick, onRefreshButtonClick}: PianoFrameProps) {
+export function PianoFrame({onHelpButtonClick, onRefreshButtonClick, onMicButtonClick}: PianoFrameProps) {
     return (
         <div className="piano-frame">
+            <Mic className="icon mic-icon" onClick={onMicButtonClick}/>
             <RefreshCw className="icon refresh-icon" onClick={onRefreshButtonClick}/>
             <HelpCircle className="icon help-icon" onClick={onHelpButtonClick}/>
         </div>
