2aa1317017c2df466ad81a755c34fb8c32ff3f1d
Tomax-21 <thom21fr@gmail.com>
2026-06-26 18:24:53 +0200

    feat : fonction pour activer le micro

--- src/utils/hooks/useAudioPitch.ts
+++ src/utils/hooks/useAudioPitch.ts
@@ -1,4 +1,29 @@
 export function useAudioPitch(isListening: boolean) {
 
     console.log("bojnour", isListening)
+
+
+    async function startMicrophone() {
+
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+        // analyseur audio natif navigateur
+        const audioContext = new AudioContext();
+
+        const source = audioContext.createMediaStreamSource(stream);
+        const analyser = audioContext.createAnalyser();
+
+        analyser.fftSize = 2048;
+        source.connect(analyser);
+
+        const buffer = new Float32Array(analyser.fftSize);
+
+        analyser.getFloatTimeDomainData(buffer);
+
+        console.log(buffer);
+
+    }
+    startMicrophone()
+
+
 }
