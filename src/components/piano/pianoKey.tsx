0daab4905b29933ea35e9fa355984715bde6e21f
Tomax-21 <thom21fr@gmail.com>
2026-06-26 14:01:50 +0200

    fix : passage du nom de la touche aux touches noires mais on affiche pas leur nom

--- src/components/piano/pianoKey.tsx
+++ src/components/piano/pianoKey.tsx
@@ -1,19 +1,20 @@
-import { areEnharmonic } from "../../utils/note_comparison";
-import type { PianoKeyProps } from "./piano_types";
+import { areEnharmonic, capitalize } from "../../utils/note_comparison";
+import { piano_key_color, type PianoKeyProps } from "./piano_types";
 
 
 
 
 export function PianoKey({name, color,active=false, target_note, onClick}: PianoKeyProps) {
     
+    console.log(name, target_note)
     if (name && target_note) {
-        active = areEnharmonic(name.toUpperCase(), target_note.toUpperCase())
+        active = areEnharmonic(capitalize(name), capitalize(target_note))
     }
 
     
     return (
         <button onClick={onClick} className={`key ${color} ${active ? 'active' : ''}`}>
-            <p>{name ?? ''}</p>
+            <p>{color !== piano_key_color.BLACK ? name : ''}</p>
         </button>
     )
 }
