7fd124be38c74507815c4a7f2ded3543b815a1e5
Tomax-21 <thom21fr@gmail.com>
2026-06-27 22:05:15 +0200

    feat : methode pour calculer le rms

--- src/utils/hooks/useAudioPitch.ts
+++ src/utils/hooks/useAudioPitch.ts
@@ -1,5 +1,13 @@
 import { useEffect, useRef } from "react";
 
+function rms(buffer: Float32Array): number {
+    let sum = 0
+    for (let i = 0; i < buffer.length; i++) {
+        sum += buffer[i] * buffer[i]
+    }
+    return Math.sqrt(sum / buffer.length)
+}
+
 export function useAudioPitch(isListening: boolean, onNoteDetected: (note: string) => void) {
 
     if (isListening)
